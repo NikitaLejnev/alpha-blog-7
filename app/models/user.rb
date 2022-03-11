b0048140bc01692a829cef2f1a7dca1305592634
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-11 12:29:54 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@
 
 # A user model with following properties:
 class User < ApplicationRecord
+  # username: required, unique case-insensitive, 3 to 25 characters
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25 }
+  # username: required, unique case-insensitive, up to 25 characters, valid
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 end
