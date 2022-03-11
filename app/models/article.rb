a91c3a73d613403815633396ab5cbea4fd40f44f
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-11 09:17:12 +0300

    Add model validation

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+# An article model with following properties:
+class Article < ApplicationRecord
+  # title: required, from 6 to 100 characters
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  # description: required, from 10 to 300 characters
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
