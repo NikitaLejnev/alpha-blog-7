09b6992efe3dc25ceccadbcc99445c7460d53245
Nikita Lezhnev <catassasin331@gmail.com>
2022-03-11 10:46:20 +0300

    Refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,9 @@
 
 # Articles controller for CRUD operations
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def show; end
 
   def index
     @articles = Article.all
@@ -14,8 +14,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit; end
+
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = 'Article created successfully'
       redirect_to @article
@@ -25,8 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = 'Article updated successfully'
       redirect_to @article
     else
@@ -35,8 +36,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
